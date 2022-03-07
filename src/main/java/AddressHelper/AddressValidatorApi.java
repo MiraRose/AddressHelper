f03628909476ce9e4c866ba029e5863f0cc008de
MiraRose <miraastar@gmail.com>
2022-03-07 16:02:29 -0500

    Sketch out of api call

---
+++ src/main/java/AddressHelper/AddressValidatorApi.java
@@ -0,0 +1,27 @@
+package AddressHelper;
+
+import AddressHelper.Model.Address;
+import com.google.common.net.UrlEscapers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AddressValidatorApi {
+
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    public HttpResponse<String> retrieveResponse(Address address, String key) throws IOException, InterruptedException, URISyntaxException {
+
+        String defaultCountryCode = "US";
+
+        String url = "https://api.address-validator.net/api/verify?StreetAddress=&CountryCode="
+                + defaultCountryCode + "&APIKey=" + key;
+        String encodedUrlString = UrlEscapers.urlFragmentEscaper().escape(url);
+        HttpRequest httpRequest = HttpRequest.newBuilder(new URI(encodedUrlString)).GET().build();
+        return client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+}
